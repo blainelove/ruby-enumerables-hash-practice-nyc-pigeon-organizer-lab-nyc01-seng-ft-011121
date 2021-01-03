df9f98a885bdd7598ed865bbdb1a5b1489535471
Blaine R Love <blainelove94@gmail.com>
2021-01-03 21:48:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require'pry'
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array|
-    #binding.pry
+
     value.each do |inner_key, names|
 
       names.each do |name|
@@ -12,8 +12,6 @@ def nyc_pigeon_organizer(data)
           !final_array[name][key] = []
         end
         final_array[name][key].push(inner_key)
-        binding.pry
-
       end
     end
     final_array
