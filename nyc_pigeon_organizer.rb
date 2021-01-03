880228fa476a494877b6f3223554ad2f0e13e313
Blaine R Love <blainelove94@gmail.com>
2021-01-03 21:38:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,9 @@ def nyc_pigeon_organizer(data)
     #binding.pry
     value.each do |inner_key, names|
       binding.pry
+      names.each do |name|
+        binding.binding.pry
+      end
     end
     final_array
    end
