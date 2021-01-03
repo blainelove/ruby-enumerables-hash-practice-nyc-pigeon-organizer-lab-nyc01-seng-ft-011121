7f642a2c535ab66a2ab0e78569df0d3fa1c0af66
Blaine R Love <blainelove94@gmail.com>
2021-01-03 21:45:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,13 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
 
       names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
         binding.pry
+    
       end
     end
     final_array
